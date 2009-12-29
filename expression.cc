1c25940872cb5ffcdabedfbd76a1506b3ffdceea
Juli Mallett <juli@clockworksquid.com>
2009-12-29 23:59:13 +0000

    Cache applications resulting from partially-productive binds.  Not sure this should work, actually, but it seems to!

--- expression.cc
+++ expression.cc
@@ -1,7 +1,5 @@
 #include <iostream>
-#ifdef MEMOIZE
 #include <map>
-#endif
 #include <ostream>
 #include <vector>
 
@@ -100,6 +98,8 @@ Expression::bind(const Name& v, const Ref<Expression>& e) const
 	case EString:
 		return (Ref<Expression>());
 	case EApply: {
+		static std::map<std::pair<unsigned, unsigned>, Ref<Expression> > apply_cache;
+
 		Ref<Expression> a(expressions_[0]);
 		Ref<Expression> b(expressions_[1]);
 
@@ -115,7 +115,18 @@ Expression::bind(const Name& v, const Ref<Expression>& e) const
 		if (b.null())
 			b = expressions_[1];
 
-		return (new Expression(a, b));
+		std::pair<unsigned, unsigned> ids(a.id(), b.id());
+		std::map<std::pair<unsigned, unsigned>, Ref<Expression> >::const_iterator it;
+
+		it = apply_cache.find(ids);
+		if (it != apply_cache.end())
+			return (it->second);
+
+		Ref<Expression> expr(new Expression(a, b));
+
+		apply_cache[ids] = expr;
+
+		return (expr);
 	}
 	case EFunction:
 		return (function_->bind(v, e));
