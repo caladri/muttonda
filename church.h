57c538bde09a8e64c761a472cce8442d68b21ae8
Juli Mallett <juli@clockworksquid.com>
2009-12-30 00:00:15 +0000

    Memoize (church).

--- church.h
+++ church.h
@@ -1,6 +1,8 @@
 #ifndef	CHURCH_H
 #define	CHURCH_H
 
+#include <map>
+
 struct ChurchBuiltin {
 	static std::string name(void)
 	{
@@ -9,15 +11,26 @@ struct ChurchBuiltin {
 
 	static Ref<Expression> function(const std::vector<Ref<Expression> >& expressions)
 	{
+		static std::map<uintmax_t, Ref<Expression> > memoized;
+		std::map<uintmax_t, Ref<Expression> >::const_iterator it;
 		Ref<Expression> a(expressions[0]);
-		unsigned i = a->scalar().value();
+		uintmax_t i = a->scalar().value();
+
+
+		it = memoized.find(i);
+		if (it != memoized.end())
+			return (it->second);
 
 		Ref<Expression> expr(new Expression(Name("x")));
 		Ref<Expression> f(new Expression(Name("f")));
 		while (i--) {
 			expr = new Expression(f, expr);
 		}
-		return (new Expression(Lambda("f", new Expression(Lambda("x", expr)))));
+		expr = new Expression(Lambda("f", new Expression(Lambda("x", expr))));
+
+		memoized[i] = expr;
+
+		return (expr);
 	}
 };
 
