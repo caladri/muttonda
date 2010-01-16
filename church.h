baa7d1f8e9590f455b6fd4e3c999885fa8f5801e
Juli Mallett <juli@clockworksquid.com>
2010-01-16 09:00:22 +0000

    Cache church numerals.

--- church.h
+++ church.h
@@ -9,8 +9,20 @@ struct ChurchBuiltin {
 
 	static Ref<Expression> function(const std::vector<Ref<Expression> >& expressions)
 	{
+		static std::map<unsigned, Ref<Expression> > expr_map, scalar_map;
+		std::map<unsigned, Ref<Expression> >::const_iterator it;
+
 		Ref<Expression> a(expressions[0]);
+		it = expr_map.find(a.id());
+		if (it != expr_map.end()) {
+			return (it->second);
+		}
+
 		uintmax_t i = a->scalar().value();
+		it = scalar_map.find(i);
+		if (it != scalar_map.end()) {
+			return (it->second);
+		}
 
 		Ref<Expression> expr(Expression::name(L"x"));
 		Ref<Expression> f(Expression::name(L"f"));
@@ -18,6 +30,9 @@ struct ChurchBuiltin {
 			expr = Expression::apply(f, expr);
 		}
 		expr = Expression::lambda(L"f", Expression::lambda(L"x", expr));
+
+		expr_map[a.id()] = expr;
+		scalar_map[i] = expr;
 
 		return (expr);
 	}
