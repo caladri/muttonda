4287b60c5f7c9b79010e703efb1b8d5d4c1c775e
Juli Mallett <juli@clockworksquid.com>
2010-02-07 05:36:47 +0000

    Use infix.

--- example/inf.hs
+++ example/inf.hs
@@ -1,7 +1,7 @@
-print-list (map unchurch (upto $0 $10))
+print-list (map unchurch $0 `..` $10)
 
-print-list (map unchurch (upto $800 $804))
+print-list (map unchurch $800 `..` $804)
 
 print-list (map unchurch (down $10))
 
-print-list (map (. unchurch fact) (down $7))
+print-list (map unchurch `.` fact (down $7))
