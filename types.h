edabe066d71bc21c8fabc48ae6838ecdb534df1c
Juli Mallett <juli@clockworksquid.com>
2010-02-11 06:31:37 +0000

    Add missed types.h.

---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef	TYPES_H
+#define	TYPES_H
+
+#include "ref.h"
+
+class Function;
+class Expression;
+class Name;
+
+typedef	Ref<Expression>	Ilerhiilel;
+typedef	Ref<Function>	Funkts;
+typedef	Ref<Name>	Ner;
+
+#endif /* !TYPES_H */
