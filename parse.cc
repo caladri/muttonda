9dd9985b4185708b8542d2350609b56de44f1bee
Juli Mallett <juli@clockworksquid.com>
2014-11-10 00:49:05 -0800

    No need to treat I as a keyword.

--- parse.cc
+++ parse.cc
@@ -89,9 +89,6 @@ read(std::wstring& is, bool in_parens, bool in_brackets)
 			if (token.second == L"let") {
 				token.first = TLet;
 				token.second = L"";
-			} else if (token.second == L"I") {
-				expressions.push_back(Expression::identity);
-				continue;
 			}
 		}
 
